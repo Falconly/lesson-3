46a6b07bf77d180ce33319ade1c203851f7152b7
Falconly <abakumov.2017@mail.ru>
2023-04-18 21:55:49 +0500

    task1

--- task1.py
+++ task1.py
@@ -1,3 +1,3 @@
-day, month, temperature = 18, 'Апреля', -1
+day, month, temperature = 18, 'Апреля', 9
 print(f'Сегодня {day} {month}. На улице {temperature} градусов.') if temperature > 0 else print(
     'Холодно, лучше остаться дома')
